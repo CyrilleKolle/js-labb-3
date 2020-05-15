5fc8a62bac6bcb0d9af7325cd4a34758de144420
CyrilleKolle <ngidekollecyrille@gmail.com>
2020-05-15 22:21:51 +0200

    successfully fetched advices from API

--- my-labb3/src/store/index.js
+++ my-labb3/src/store/index.js
@@ -5,10 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    advice: null
   },
   mutations: {
   },
   actions: {
+    fetchAdvice(){
+      fetch('https://api.adviceslip.com/advice')
+      .then((response) => response.json())
+      .then((result) => this.commit('anAdvice', result))
+    }
   },
   modules: {
   }
--- my-labb3/src/views/Home.vue
+++ my-labb3/src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <HelloWorld msg="Welcome to Advice.App" />
+
+    <p :key="advice.id" v-for="advice in advices">{{advice.advice}}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
+  },
+  created() {
+    fetch("https://api.adviceslip.com/advice")
+      .then(response => response.json())
+      .then(result => {
+        this.advices = result;
+      });
+  },
+  data() {
+    return {
+      advices: null
+    };
   }
-}
+};
 </script>
